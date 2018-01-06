1976cccc25a8664086d65f03a5b0f8402fa5c1cd
ikostan <igorkostan@gmail.com>
2018-01-05 19:52:34 -0800

    FileManager -> debug code commented out

--- file_manager.py
+++ file_manager.py
@@ -25,21 +25,21 @@ class FileManager:
                 std_data = {}
                 items = line.split('; ')
                 # DEBUG
-                print('items -> {}'.format(items))
+                # print('items -> {}'.format(items))
                 for item in items:
                     params = item.split(': ')
                     # DEBUG
-                    print('params -> {}'.format(params))
+                    # print('params -> {}'.format(params))
                     std_data[params[0]] = params[1].strip()
 
                 try:
                     # DEBUG
-                    print('std_data -> {}'.format(std_data))
+                    # print('std_data -> {}'.format(std_data))
                     students.append(Student(std_data))
                 except Exception as e:
                     print(e)  # print error
                     # Debug:
-                    print(type(std_data))
+                    # print(type(std_data))
                     for d in std_data.items():
                         print(d)
                     print()
